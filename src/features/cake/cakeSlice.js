b0ca4e88a3a1b2d945157e3a14079bf4d66dfbde
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-05-29 16:43:59 +0530

    feat: initiate redux store and implement cakeSlice

---
+++ src/features/cake/cakeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState,
+  reducers: {
+    ordered: (state) => state.numOfCakes--,
+    restocked: (state, action) => (state.numOfCakes += action.payload),
+  },
+});
+const { ordered, restocked } = cakeSlice.actions;
+
+export default cakeSlice.reducer;
+export { ordered, restocked };
